ac894d33676a884e95d7c56fbc7bfee1e3ae3ae9
vagrant <vagrant@homestead>
2018-03-01 08:28:34 +0000

    主题页面

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -39,7 +39,7 @@ class TopicsController extends Controller
         $topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '成功创建话题！');
 	}
 
 	public function edit(Topic $topic)
@@ -53,7 +53,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '更新成功！');
 	}
 
 	public function destroy(Topic $topic)
@@ -61,7 +61,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('message', '成功删除！');
 	}
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'IdeBBS') - Laravel 进阶教程</title>
-
+    <meta name="description" content="@yield('description', 'IdeBBS 爱好者社区')" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('styles')

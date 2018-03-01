bc5609a1577ff134b70e013f887a3cafae4c7b7d
vagrant <vagrant@homestead>
2018-03-01 03:08:01 +0000

    用户基础信息编辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -11,5 +12,16 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show',compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(UserRequest $request,User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
     }
 }

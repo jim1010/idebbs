894c34b2f90cbfb460a019b7b1a8e9eaf6208846
vagrant <vagrant@homestead>
2018-03-05 07:30:34 +0000

    Add manage contents permission and visit horizon dashboard permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,5 +29,9 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        \Horizon::auth(function ($request) {
+            // 是否是站长
+            return \Auth::user()->hasRole('Founder');
+        });
     }
 }

ed25d441991adca315397110eae9f68ea127ccbc
vagrant <vagrant@homestead>
2018-03-16 03:49:49 +0000

    最近活跃时间

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,7 @@ class User extends Authenticatable
 
     use HasRoles;
     use Traits\ActiveUserHelper;
+    use Traits\LastActivedAtHelper;
 
     /**
      * The attributes that are mass assignable.
